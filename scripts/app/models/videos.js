2380ec979671837c3b5e9e14dd03cb594dcbbbd2
dlarbi <dlarbi@purdue.edu>
2015-02-10 03:00:23 -0800

    A few improvements to the example photo gallery

--- scripts/app/models/videos.js
+++ scripts/app/models/videos.js
@@ -3,24 +3,16 @@ define(["app/events", "app/models/BaseModel"], function(Events, BaseModel) {
   function Video_Model() {
     BaseModel.call(this);
   }
-
   Video_Model.prototype = Object.create(BaseModel.prototype);
+
+
   Video_Model.prototype.initialize = function() {
     this.endpoint = 'http://jsonplaceholder.typicode.com/posts/8';
     $.extend(this, Events);
   }
-  Video_Model.prototype.fetch = function() {
-    var self = this;
-    $.ajax({
-      url: self.endpoint,
-      success:function(data) {
-        self.setState(data)
-      }
-    });
-    return;
-  }
+
+
   Video_Model.prototype.constructor = Video_Model;
   var Model = Model || new Video_Model();
   return Model;
-
 });
--- scripts/app/views/photo_page.js
+++ scripts/app/views/photo_page.js
@@ -3,13 +3,11 @@ define(["app/events", "app/views/BaseView"], function(Events, BaseView) {
   function Photos_View() {
     BaseView.call(this);
   }
-
   Photos_View.prototype = Object.create(BaseView.prototype);
 
   Photos_View.prototype.render = function() {
-
     var htmlOut = this.Templating.buildTemplate(
-      '<div class="container"><div id="next-page">Next</div>'+
+      '<div class="container"><div id="photos-prev-page">Previous</div><div id="photos-next-page">Next</div>'+
         '<% for(var index in this) { %>'+
           '<div class="col-sm-2" style="padding:20px 0;"><img width="100%" src="<% this[index]["thumbnailUrl"] %>"/></div>'+
         '<% } %>'+
@@ -19,10 +17,23 @@ define(["app/events", "app/views/BaseView"], function(Events, BaseView) {
 
     this.el.html(htmlOut);
     $('#main-content').html(this.el);
+    this.bindEvents();
   }
 
-  Photos_View.prototype.constructor = Photos_View;
+  Photos_View.prototype.bindEvents = function() {
+    var self = this;
+    $('#photos-next-page').off('click').on('click', function() {
+      var i = self.model.getCurrentAlbum();
+      self.model.setCurrentAlbum(i+1);
+    });
+    $('#photos-prev-page').off('click').on('click', function() {
+      var i = self.model.getCurrentAlbum();
+      self.model.setCurrentAlbum(i-1);
+    });
+  }
 
+
+  Photos_View.prototype.constructor = Photos_View;
   var View = View || new Photos_View();
   return View;
 
