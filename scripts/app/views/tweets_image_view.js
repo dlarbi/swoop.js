ac4969f219a8af787e42ebf1530e0fd51ac0b9be
dlarbi <dlarbi@purdue.edu>
2015-02-28 04:40:35 -0800

    removed some redundant methods

--- scripts/app/views/tweets_image_view.js
+++ scripts/app/views/tweets_image_view.js
@@ -3,7 +3,6 @@ define(["app/views/BaseView"], function(BaseView) {
   var Tweets_Image_View = BaseView.extend({
 
     render : function(DOMElement) {
-      console.log(this.model.attributes)
       var htmlOut = this.Templating.buildTemplate(
         '<div style="top:30%; left:15%; background:#fff; position:absolute; padding:40px; width:300px; margin-left:-200px; height:400px;" id="tweets-images-box">'+
           '<div style="width:100%;height:75%;overflow:auto; overflow-x:hidden; text-align:center;">'+
@@ -18,14 +17,8 @@ define(["app/views/BaseView"], function(BaseView) {
       );
       this.el.html(htmlOut)
       $(DOMElement).append(this.el);
-    },
-
-    refreshFromCurrentGeo : function() {
-      require(["app/controllers/map_controller"], function(Controller) {
-        Controller.refresh_tweet_map(View.model, View.map.getCenter());
-      })
     }
-
+    
   });
 
   var View = View || new Tweets_Image_View();
